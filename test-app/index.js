e66b2fb093ec42f3a9838283692aa2973c904616
antelle <dmitryvit@gmail.com>
2020-12-25 05:02:33 +0100

    fixing linux build

--- test-app/index.js
+++ test-app/index.js
@@ -15,7 +15,7 @@ function createWindow() {
 app.whenReady().then(() => {
     console.log('Test app started');
     setTimeout(() => process.exit(0), 5000);
-    process.stdin.on('data', () => process.exit(0));
+    //process.stdin.on('data', () => process.exit(0));
 
     createWindow();
 });

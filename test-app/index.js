4378d2a50dc17ec0f4ee7e0001f09bc53aeceb86
antelle <dmitryvit@gmail.com>
2020-12-25 05:56:44 +0100

    fixing linux build

--- test-app/index.js
+++ test-app/index.js
@@ -1,28 +1,3 @@
-const { app, BrowserWindow } = require('electron');
-
-let win;
-
-function createWindow() {
-    win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            contextIsolation: false
-        }
-    });
-}
-
-app.whenReady().then(() => {
-    console.log('Test app started');
-    setTimeout(() => win.close(), 5000);
-    process.stdin.on('data', () => win.close());
-
-    createWindow();
-});
-
-app.on('window-all-closed', () => {
-    if (win) {
-        win = null;
-    }
-    app.quit();
-});
+console.log('Test app started');
+setTimeout(() => process.exit(0), 5000);
+process.stdin.on('data', () => process.exit(0));
